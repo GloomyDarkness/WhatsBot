f012e1d51d01c6a1587df31aebfb1bf64e33616f
GloomyDarkness <miaaumiau748@gmail.com>
2022-07-14 21:33:12 -0300

    erro arrumado

--- src/events/message/message_create.js
+++ src/events/message/message_create.js
@@ -3,7 +3,7 @@ module.exports = async (client, message) => {
     const prefix = '!';
     if (message.isForwarded) return
     if (message.fromMe || !message.body.startsWith(prefix)) return
-    if (!message.isGroup) return client.send(message.from, "Olá, minhas funções funcionam apenas em grupo!")
+    if (!message.isGroup) return client.sendMessage(message.from, "Olá, minhas funções funcionam apenas em grupo!")
 
     const args = message.body.slice(prefix.length).trim().split(/ +/g)
     const cmdName = args.shift()?.toLowerCase();
